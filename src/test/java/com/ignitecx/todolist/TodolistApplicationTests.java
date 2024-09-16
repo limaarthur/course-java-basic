af1b08e29112112add238f9def65c025d602c72d
Arthur Lima <artglima.dev@gmail.com>
2024-09-16 16:33:53 -0300

    first commit - project structure created

---
+++ src/test/java/com/ignitecx/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ignitecx.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
