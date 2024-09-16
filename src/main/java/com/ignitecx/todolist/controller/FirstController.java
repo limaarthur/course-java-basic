6b0dbb2ea85cf5b7864d3480c1cb812f1d3032a8
Arthur Lima <artglima.dev@gmail.com>
2024-09-16 18:28:42 -0300

    creating the controller

---
+++ src/main/java/com/ignitecx/todolist/controller/FirstController.java
@@ -0,0 +1,17 @@
+package com.ignitecx.todolist.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("firstRoute")
+public class FirstController {
+
+    @GetMapping("/firstMethod")
+    public String firstMessage() {
+        return "Funcionou";
+    }
+}
+
+
