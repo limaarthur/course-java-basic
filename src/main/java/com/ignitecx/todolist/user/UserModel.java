dc497a3b07933cce0a66c501ad0b616ce329dd28
Arthur Lima <artglima.dev@gmail.com>
2024-09-18 16:46:41 -0300

    validating the user

--- src/main/java/com/ignitecx/todolist/user/UserModel.java
+++ src/main/java/com/ignitecx/todolist/user/UserModel.java
@@ -1,5 +1,6 @@
 package com.ignitecx.todolist.user;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -17,6 +18,7 @@ public class UserModel {
     @GeneratedValue(generator = "UUID")
     private UUID id;
 
+    @Column(unique = true)
     private String username;
     private String name;
     private String password;
