7811353bb66b60637b684bd85396a6cb99cdfd70
Arthur Lima <artglima.dev@gmail.com>
2024-09-24 17:02:33 -0300

    password decode

--- src/main/java/com/ignitecx/todolist/filter/FilterTaskAuth.java
+++ src/main/java/com/ignitecx/todolist/filter/FilterTaskAuth.java
@@ -1,9 +1,12 @@
 package com.ignitecx.todolist.filter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import com.ignitecx.todolist.user.UserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,6 +15,9 @@ import java.util.Base64;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -27,11 +33,19 @@ public class FilterTaskAuth extends OncePerRequestFilter {
         String[] credentials = authString.split(":");
         String username = credentials[0];
         String password = credentials[1];
-        System.out.println("Authorization");
-        System.out.println(username);
-        System.out.println(password);
 
-        filterChain.doFilter(request, response);
+        var user = this.userRepository.findByUsername(username);
+        if (user == null) {
+            response.sendError(401);
+        } else {
+            var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+            if (passwordVerify.verified) {
+                filterChain.doFilter(request, response);
+            } else {
+                response.sendError(401);
+            }
 
+
+        }
     }
 }
